b5d1b2d07f0722bdfc11e37cb453fe9a94350020
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-07-22 18:30:45 -0400

    changed from dictionary back to list in EventController. added Edit view, added a route to Edit in Index view. Edit in controller gets info by ID, submitediteventform in controller updates the information.

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +10,11 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        static private List<Event> Events = new List<Event>();
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
 
@@ -26,9 +28,42 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string description)
+        public IActionResult NewEvent(Event newEvent)
         {
-            Events.Add(name, description);
+            EventData.Add(newEvent);
+            return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach(int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
+            return Redirect("/Events");
+        }
+
+        [Route("/Events/Edit/{eventId}")]
+        public IActionResult Edit(int eventId)
+        {
+            ViewBag.eventToEdit = EventData.GetById(eventId);
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Edit/")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            EventData.GetById(eventId).Name = name;
+            EventData.GetById(eventId).Description = description;
             return Redirect("/Events");
         }
     }
